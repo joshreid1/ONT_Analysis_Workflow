9efc1d33102af1b6c20e8ecad08cb9c8f60a2dee
Josh Reid <reid.j@wehi.edu.au>
2024-08-12 14:23:12 +1000

    Create basecalling.sh

---
+++ basecalling.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition gpuq     # submit to the gpuq partition
+#SBATCH --cpus-per-task 6   # request 6 CPUs
+#SBATCH --mem 64G         # request 64GB
+#SBATCH --gres gpu:A30:4     # requesting 4 x A30 GPU
+#SBATCH --job-name <sample_basecalling>
+
+module load dorado/0.7.0
+
+dorado basecaller /stornext/System/data/nvidia/dorado/models/dna_r10.4.1_e8.2_400bps_sup@v5.0.0 \
+--modified-bases 5mCG_5hmCG --reference /vast/projects/bahlo_epilepsy/ref_genomes/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
+./path_to_all_pod5 > <sample>_sup_v5.0.0_5mC_5hmC_aligned.bam
+
+samtools sort -@ 6 -o <sample>_sup_v5.0.0_5mC_5hmC_sorted.bam <sample>_sup_v5.0.0_5mC_5hmC_aligned.bam
